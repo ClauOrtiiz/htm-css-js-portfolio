54dddd6146da085fb54d1503cf523b4a4e6c3582
Claudia Ortiz LLamoca <holaclaudiia@gmail.com>
2023-11-22 12:55:34 -0500

    UPDATE: header

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import React from 'react';
-import Header  from './componentes/header/header';
+import Header  from './componentes/header/Header';
 import { Home } from './componentes/home/home';
 import { About } from './componentes/about/about';
 import { Skills } from './componentes/skills/skills';
