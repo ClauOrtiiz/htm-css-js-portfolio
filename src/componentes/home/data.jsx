e6eeb924580bddaf09ee149c12ac9f19e327cb9a
Claudia Ortiz LLamoca <holaclaudiia@gmail.com>
2024-02-28 17:36:00 -0500

    UPDATE:subtitle

--- src/componentes/home/data.jsx
+++ src/componentes/home/data.jsx
@@ -6,6 +6,7 @@ export const Data = () => {
         <div className="home__data">
             <h1 className="home__title">Claudia Ortiz Llamoca</h1>
             <h3 className="home__subtitle">Developer Web Frontend</h3>
+            <h3 className="home__subtitle">Diseñadora UX/UI</h3>
             <p className="home__description">
                 Me encanta convivir en el mundo digital creando experiencias
                 y diseños funcionales.</p>
